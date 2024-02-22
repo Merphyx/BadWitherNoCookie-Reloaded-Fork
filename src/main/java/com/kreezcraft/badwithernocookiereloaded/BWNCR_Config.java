5e0224a0d864291c96626700b3a54b6ead3a7d77
Merphyx <merphyx@gmail.com>
2024-02-22 01:47:33 +0100

    Update BWNCR_Config.java

--- src/main/java/com/kreezcraft/badwithernocookiereloaded/BWNCR_Config.java
+++ src/main/java/com/kreezcraft/badwithernocookiereloaded/BWNCR_Config.java
@@ -27,7 +27,7 @@ public class BWNCR_Config implements ConfigData {
 		@Comment("Silence the server-wide Thunder broadcast sound caused by the Lightning event")
 		public boolean silenceLightning = true;
 
-		@Comment("A list of sounds to silence, discoverable with the toggle command /listen enter one sound event per line with no commas.")
+		@Comment("A list of sounds to silence, discoverable with the toggle command /bwncr listen enter one sound event per line with no commas.")
 		public List<String> silenceUs = new ArrayList<>();
 
 		@Comment("If enabled the console will load up spam showing what sounds are being received and whether or not they are being canceled")
